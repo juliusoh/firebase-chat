adde7f58218b4ae18dfd79fa2295a9d109a2264b
Julius Oh <juliusoh@gmail.com>
2022-09-07 22:06:33 -0700

    ui design

---
+++ src/pages/Register.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Add from "../img/addAvatar.png";
+
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Google Firebase Chat</span>
+        <span className="title">Register</span>
+        <form action="">
+          <input type="text" placeholder="Display Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add Avatar</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
